7577e9f04e230164a0b132f34c88e6f18317dbc4
Michael Klett <michael@webadvocate.com>
2009-02-02 15:13:16 -0500

    Implement a +format_nested_set+ helper method for displaying an HTML representation of a nested set.

--- test/awesome_nested_set/helper_test.rb
+++ test/awesome_nested_set/helper_test.rb
@@ -5,6 +5,7 @@ module CollectiveIdea
     module NestedSet #:nodoc:
       class AwesomeNestedSetTest < Test::Unit::TestCase
         include Helper
+        include ActionView::Helpers::TagHelper
         fixtures :categories
         
         def test_nested_set_options
@@ -33,6 +34,30 @@ module CollectiveIdea
             "#{'-' * c.level} #{c.name}"
           end
           assert_equal expected, actual
+        end
+        
+        def test_format_nested_set_with_roots
+          expected = %{<ul class="tree"><li>Top Level<ul class="tree"><li>Child 1</li><li>Child 2<ul class="tree"><li>Child 2.1</li></ul></li><li>Child 3</li></ul></li><li>Top Level 2</li></ul>}
+          actual = format_nested_set(Category.roots) {|c| c.name}
+          assert_dom_equal expected, actual
+        end
+        
+        def test_format_nested_set_with_first_root
+          expected = %{<ul class="tree"><li>Top Level<ul class="tree"><li>Child 1</li><li>Child 2<ul class="tree"><li>Child 2.1</li></ul></li><li>Child 3</li></ul></li></ul>}
+          actual = format_nested_set(Category.root) {|c| c.name}
+          assert_dom_equal expected, actual
+        end
+        
+        def test_format_nested_set_with_class_options
+          expected = %{<ul><li class="leaf">Top Level<ul><li class="leaf">Child 1</li><li class="leaf">Child 2<ul><li class="leaf">Child 2.1</li></ul></li><li class="leaf">Child 3</li></ul></li></ul>}
+          actual = format_nested_set(Category.root, :major => {:tag => :ul, :class => nil}, :minor => {:tag => :li, :class => 'leaf'}) {|c| c.name}
+          assert_dom_equal expected, actual
+        end
+        
+        def test_format_nested_set_with_custom_tags
+          expected = %{<div><span>Top Level<div><span>Child 1</span><span>Child 2<div><span>Child 2.1</span></div></span><span>Child 3</span></div></span></div>}
+          actual = format_nested_set(Category.root, :major => {:tag => :div}, :minor => {:tag => :span}) {|c| c.name}
+          assert_dom_equal expected, actual
         end
         
       end
